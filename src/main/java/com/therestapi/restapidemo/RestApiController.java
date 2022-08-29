4649495da5b7986d103d2de21f68aa15d0158b3b
Alexander <a.kan4988@gmail.com>
2022-08-29 22:48:37 +0300

    [P]@DeleteMapping .removeIf()

--- src/main/java/com/therestapi/restapidemo/RestApiController.java
+++ src/main/java/com/therestapi/restapidemo/RestApiController.java
@@ -1,9 +1,7 @@
 package com.therestapi.restapidemo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,8 +10,9 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @RestController
- class RestApiController {
+class RestApiController {
     private List<Coffee> coffeeList = new ArrayList<>();
+
     public RestApiController() {
         coffeeList.addAll(List.of(
                 new Coffee("Cafee Grajd"),
@@ -24,17 +23,47 @@ import java.util.stream.Stream;
     }
 
     @GetMapping("/coffee")
-    Iterable<Coffee>getCoffee(){
+    Iterable<Coffee> getCoffee() {
         return coffeeList;
     }
 
     @GetMapping("/coffee/{id}")
-    Optional<Coffee> getCofeeById(@PathVariable String id){
-       for (Coffee c: coffeeList){
-           if(c.getId().equals(id)){
-               return Optional.of(c);
-           }
-       }
-       return Optional.empty();
+    Optional<Coffee> getCoffeeById(@PathVariable String id) {
+        for (Coffee c : coffeeList) {
+            if (c.getId().equals(id)) {
+                return Optional.of(c);
+            }
+        }
+        return Optional.empty();
     }
+
+    @PostMapping("/coffee")
+    @ResponseStatus
+    Coffee postCoffee(@RequestBody Coffee coffee) {
+        coffeeList.add(coffee);
+        return coffee;
+    }
+
+
+    //TODO INDEXOF https://vertex-academy.com/tutorials/ru/indexof-java/
+    @PutMapping("/coffee/{id}")
+    Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+        int coffeeIndex = -1;
+
+        for (Coffee c : coffeeList) {
+            if (coffee.getId().equals(id)) {
+                coffeeIndex = coffeeList.indexOf(c);
+                coffeeList.set(coffeeIndex, coffee);
+
+            }
+        }
+        return (coffeeIndex == -1) ? postCoffee(coffee) : coffee;
+    }
+
+    @DeleteMapping("/coffee/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    void deleteCoffee(@PathVariable String id){
+        coffeeList.removeIf(c->c.getId().equals(id));
+    }
+
 }
